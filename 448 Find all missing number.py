fd4298ce2998138f0f42772841cf9eba9b5fc7d6
Whitelister <spikenotfound731@gmail.com>
2026-08-18 22:23:12 +0530

    Optimal sol for 448

--- 448 Find all missing number.py
+++ 448 Find all missing number.py
@@ -26,7 +26,25 @@ class Solution_Brute:
             expected += 1
         return missing        
         
+        
 # Local Testing
-sol_brute = Solution_Brute()
+# sol_brute = Solution_Brute()
 array1 = [4,3,2,7,8,2,3,1]
-print(sol_brute.findDisappearedNumbers(array1))
+# print(sol_brute.findDisappearedNumbers(array1))
+
+# Optimal Solution
+
+class Solution_Optimal:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        set_nums = set(nums)
+        ret =[]
+        for i in range(1, len(nums)+1):
+            if i not in set_nums:
+                ret.append(i)
+        return ret
+        
+        
+# Local Testing
+sol_optimal = Solution_Optimal()
+array2 = array1
+print(sol_optimal.findDisappearedNumbers(array2))
