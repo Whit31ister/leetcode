e86a4ae5cdbae7a7d0a563689adc019012f4df2e
Whitelister <spikenotfound731@gmail.com>
2026-08-21 10:29:12 +0530

    Vecots output stuff

--- Cpp/Array/STL/Algorithms.cpp
+++ Cpp/Array/STL/Algorithms.cpp
@@ -40,13 +40,38 @@ void explainVector(){
     vector<int> v1(5, 20);
     vector<int> v2(v1);
 
+    // Iterator
     vector<int>::iterator it = v.begin();
 
+
+    // Output of vector
     it++;
     cout << *(it) << " ";
 
-}
+    it = it + 2;
+    cout << *(it) << " ";
 
+    // vector<int>::iterator it = v.end();
+    // vector<int>::iterator it = v.rend();
+    // vector<int>::iterator it = v.rbegin();
+
+    cout << v[0] << " " << v.at(0);
+    cout << v.back() << " ";
+
+    for(vector<int>::iterator it = v.begin(); it != v.end(); it++){
+        cout << *(it) << " ";
+    }
+
+    for (auto it = v.begin(); it != v.end(); it++){
+        cout << *(it) << " ";
+    }
+
+    for (auto it : v){
+        cout << it << " ";
+    }
+
+
+}
 
 // Main func
 int main(){
